d1361cd30fc7134b85772caad7454ce35410c5bc
Santiago Sánchez <icisan27@gmail.com>
2021-10-18 18:24:44 +0200

    Ancho y alto de la ventana. Título de la ventana. Color del fondo de la venbtana.

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ from main_functions import *
 pygame.init()
 
 #call main routine
-size = [1300, 600] # Define size of windows
-main2(size)
+size = [1300,600]
+ancho = int(input("Ancho de la ventana: "))
+alto = int(input("Alto de la ventana: "))
+size = (ancho,alto)
+titulo = input("¿Cómo quieres que se llame la ventana? ")
+rojo = int(input("Cantidad de rojo: "))
+verde = int(input("Cantidad de verde: "))
+azul = int(input("Cantidad de azul: "))
+color = (rojo, verde, azul)
+main2(size,titulo,color)
